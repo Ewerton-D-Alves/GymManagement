54893cffffe84cd6e7a3ff7aec3b7fdd70c46760
Ewerton Alves <ewertondanilo13@gmail.com>
2026-06-09 09:37:21 -0300

    criação das tabelas na classe 'Pessoa'

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,11 @@
-
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
 
-
+        Scanner sc = new Scanner(System.in);
+        System.out.println("======= Bem-vindo a academia entra forte e sai frango ======");
+        System.out.println("Para consultar o menu, digite: 1 \nPara sair, digite: 0");
+        int primeiroValor = sc.nextInt();
     }
 }
